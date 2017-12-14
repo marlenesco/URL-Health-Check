8ed1924f19492c03ec68592ba93873b29b673b98
David Foliti <david.foliti@verisure.it>
2017-12-14 13:05:49 +0100

    first commit

---
+++ inc/conf.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+class Config
+{
+    const
+
+        SMTP_SERVER         = 'smtp.gmail.com',
+        SMTP_PORT           = 25,
+        SMTP_SECURITY       = 'TLS',
+        SMTP_USER           = 'your_account@gmail.com',
+        SMTP_PASSWORD       = 'your_password',
+
+        HC_EMAIL_SUBJECT  = 'Houston, we have a problem',
+        HC_EMAIL_TO       = [
+                                'john.doe@example.com',
+                                'agent.smith@example.com',
+                                'marlenesco@gmail.com',
+                            ]
+    ;
+
+}
+
+
+
+
